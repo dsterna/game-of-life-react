e02d4b4a6746300dd14ef5448be56a16468391ce
dsterna <dsterna@kth.se>
2021-04-25 22:40:48 +0200

    deploying to github pages

---
+++ docs/precache-manifest.3d91a4cad568f3b06863162b0e63f978.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e93ab7714d9513fe1fc0d2640ece700",
+    "url": "https://dsterna.github.io/game-of-life-react/index.html"
+  },
+  {
+    "revision": "187a6babc86ea2284ea7",
+    "url": "https://dsterna.github.io/game-of-life-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c23dad43f8e46365eb1b",
+    "url": "https://dsterna.github.io/game-of-life-react/static/js/2.a3db42d5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "https://dsterna.github.io/game-of-life-react/static/js/2.a3db42d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "187a6babc86ea2284ea7",
+    "url": "https://dsterna.github.io/game-of-life-react/static/js/main.05ccdeb5.chunk.js"
+  },
+  {
+    "revision": "4b9b403349df088b7467",
+    "url": "https://dsterna.github.io/game-of-life-react/static/js/runtime-main.c5ec9398.js"
+  }
+]);
